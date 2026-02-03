b13e4790112bfe1c30538d2d8e9a235c0c858d03
Emmanuel Cruz <155833447+emmanuel-cruz-dev@users.noreply.github.com>
2026-02-03 07:04:37 -0300

    chore: removes previous folders and adds nextjs

--- src/services/authContext.ts
+++
@@ -1,6 +0,0 @@
-import { createContext } from "react";
-import { AuthContextType } from "../store/authStore";
-
-export const AuthContext = createContext<AuthContextType | undefined>(
-  undefined
-);
